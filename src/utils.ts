cc0b5dd4ecec87b06602cd3170dbe6425b0a8d04
Charles Olupot <olupotcharles@gmail.com>
2021-11-24 17:34:30 +0300

    Allow to read sqlviews

--- src/utils.ts
+++ src/utils.ts
@@ -6,6 +6,30 @@ export const enrollmentCounts = (
   const dateQuery = startDate && endDate ? `and created between '${startDate}' and '${endDate}'` : "";
   return {
     type: "QUERY",
+    "publicAccess": "rwrw----",
+    "access": {
+      "read": true,
+      "update": true,
+      "externalize": true,
+      "delete": true,
+      "write": true,
+      "manage": true,
+      "data": {
+        "read": true,
+        "write": true
+      }
+    },
+    "sharing": {
+      "owner": "w29gG6lD26U",
+      "external": false,
+      "users": {
+
+      },
+      "userGroups": {
+
+      },
+      "public": "rwrw----"
+    },
     id: "kIEqe77I6oC",
     sqlQuery: `select storedby,COUNT(programinstanceid) from programinstance where storedby in (${logins}) ${dateQuery} group by storedby;`,
     description: "Query Enrollments by Users",
@@ -21,6 +45,30 @@ export const eventCounts = (
   const dateQuery = startDate && endDate ? `and created between '${startDate}' and '${endDate}'` : "";
   return {
     type: "QUERY",
+    "publicAccess": "rwrw----",
+    "access": {
+      "read": true,
+      "update": true,
+      "externalize": true,
+      "delete": true,
+      "write": true,
+      "manage": true,
+      "data": {
+        "read": true,
+        "write": true
+      }
+    },
+    "sharing": {
+      "owner": "w29gG6lD26U",
+      "external": false,
+      "users": {
+
+      },
+      "userGroups": {
+
+      },
+      "public": "rwrw----"
+    },
     id: "kCt1rIMGkJb",
     sqlQuery: `select storedby,COUNT(programstageinstanceid) from programstageinstance where storedby in (${logins}) ${dateQuery} group by storedby;`,
     description: "Events Per Person",
